40df927d31b3a79f4e42a88a7f14a7ad686adf01
Andrey Verenya <verenya08@gmail.com>
2024-02-22 15:15:52 +0300

    Reformat

--- include/init.h
+++ include/init.h
@@ -6,7 +6,7 @@
 #include "scl_types.h"
 #include "scl_struct.h"
 
-void scl_print_error(SCL* scl);
+void scl_print_error(SCL *scl);
 
 #define CHECK_AND_PRINT(SCL_STRUCT, FUNC) { \
     scl_status check_status = FUNC; if (check_status != SCL_SUCCESS) \
@@ -18,16 +18,16 @@ void scl_print_error(SCL* scl);
     { return check_status; } \
 }
 
-SCL* create();
+SCL *create();
 
 scl_status init(SCL *scl, scl_number target_platform_number, scl_number target_device_number);
 
-scl_status get_platform_id(SCL* scl, scl_number target_platform_number);
-scl_status get_device_id(SCL* scl, scl_number target_device_number);
+scl_status get_platform_id(SCL *scl, scl_number target_platform_number);
+scl_status get_device_id(SCL *scl, scl_number target_device_number);
 
-scl_status get_platform_name(SCL* scl);
-scl_status get_device_name(SCL* scl);
+scl_status get_platform_name(SCL *scl);
+scl_status get_device_name(SCL *scl);
 
-void scl_free(SCL* scl);
+void scl_free(SCL *scl);
 
 #endif //SIMPLE_OPENCL_C_LIB_INIT_H
--- include/scl_struct.h
+++ include/scl_struct.h
@@ -10,14 +10,14 @@ typedef struct SCL_Struct {
     cl_uint num_platforms;
     cl_platform_id *platforms;
     cl_platform_id platform_id;
-    char* platform_name;
+    char *platform_name;
 
     cl_uint num_devices;
     cl_device_id *devices;
     cl_device_id device_id;
-    char* device_name;
+    char *device_name;
 
-    char* error;
+    char *error;
 } SCL;
 
 #endif //SIMPLE_OPENCL_C_LIB_SCL_STRUCT_H
